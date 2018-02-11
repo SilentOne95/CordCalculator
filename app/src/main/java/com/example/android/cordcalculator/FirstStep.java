583c17662f565660622b5d1a2ee81636baf23e05
SilentOne <SilentOne@onet.pl>
2018-02-11 15:49:41 +0100

    Preparing re-usable layouts. Little renaming.

--- app/src/main/java/com/example/android/cordcalculator/FirstStep.java
+++ app/src/main/java/com/example/android/cordcalculator/FirstStep.java
@@ -1,16 +1,9 @@
 package com.example.android.cordcalculator;
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.DashPathEffect;
-import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
 
-public class FirstStepActivity extends AppCompatActivity {
+public class FirstStep extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/android/cordcalculator/StartingScreenActivity.java
+++ app/src/main/java/com/example/android/cordcalculator/StartingScreenActivity.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class StartingScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_starting_screen);
     }
 
     public void openFirstStep(View view) {
-        Intent i = new Intent(this, FirstStepActivity.class);
+        Intent i = new Intent(this, MainLayoutActivity.class);
         startActivity(i);
     }
 }
